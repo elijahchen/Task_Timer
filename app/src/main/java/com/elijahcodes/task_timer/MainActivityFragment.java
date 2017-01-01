118a7e3d7f7034d05687bba6f5232d9489a8a726
elijahchen <code.elijahchen@gmail.com>
2016-12-31 23:55:39 -0500

    - Added Fragment to MainActivity with corresponding layout

---
+++ app/src/main/java/com/elijahcodes/task_timer/MainActivityFragment.java
@@ -0,0 +1,23 @@
+package com.elijahcodes.task_timer;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Elijah on 12/31/2016.
+ */
+
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
