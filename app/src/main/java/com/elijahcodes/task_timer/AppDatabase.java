fb63e8f6ee824f320c8a4da3436bc73a9cb2a4e4
elijahchen <code.elijahchen@gmail.com>
2017-01-02 04:08:34 -0500

    - Implemented class to initialize the SQLite database

---
+++ app/src/main/java/com/elijahcodes/task_timer/AppDatabase.java
@@ -0,0 +1,64 @@
+package com.elijahcodes.task_timer;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Elijah on 1/2/2017.
+ *
+ * Basic database class for the application
+ *
+ * There should be only one class that uses this, AppProvider.
+ */
+
+class AppDatabase extends SQLiteOpenHelper{
+    private static final String TAG = "AppDatabase";
+
+    public static final String DATABASE_NAME = "TaskTimer.db";
+    public static final int DATABASE_VERSION = 1;
+
+    //Requires constructor, implements AppDatabase as a singleton
+    private static AppDatabase instance = null;
+
+    private AppDatabase(Context context) {
+        // null is passed, custom cursor is not needed
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        Log.d(TAG, "AppDatabase: Constructor");
+    }
+
+    /**
+     * Get an instance of the applications singleton database helper object
+     * @param context the content providers context
+     * @return a SQLite database helper object
+     */
+    static AppDatabase getInstance(Context context){
+        if(instance == null){
+            Log.d(TAG, "getInstance: Creating new instance");
+            instance = new AppDatabase(context);
+        }
+        return instance;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        Log.d(TAG, "onCreate: starts");
+        String sSQL; // Use a string variable to facilitate logging
+        sSQL = "CREATE TABLE " + TasksContract.TABLE_NAME + " ("
+                + TasksContract.Columns._ID + " INTEGER PRIMARY KEY NOT NULL, "
+                + TasksContract.Columns.TASKS_NAME + " TEXT NOT NULL, "
+                + TasksContract.Columns.TASKS_DESCRIPTION + " TEXT, "
+                + TasksContract.Columns.TASKS_SORTORDER + " INTEGER);";
+
+        Log.d(TAG, "onCreate: sSQL");
+        db.execSQL(sSQL);
+
+        Log.d(TAG, "onCreate: ends");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
